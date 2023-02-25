3319e0639dcacb1e0ae7a98d2edca345bf4f28c2
osw <osw@hogegdsf.com>
2023-02-26 02:40:09 +0900

    ケース追加

--- sample_test.go
+++ sample_test.go
@@ -48,6 +48,11 @@ func TestSample(t *testing.T) {
 			v:    7,
 			want: 8,
 		},
+		{
+			name: "test9",
+			v:    8,
+			want: 9,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
